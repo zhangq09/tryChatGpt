deddbe2673d5080bcaa6f947b12c58196f399e28
zhangq09 <zq969811@gami.com>
2023-03-31 10:11:26 +0800

    first commit

---
+++ src/main/java/com/trychatgpt/study/TryChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.trychatgpt.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryChatGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryChatGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trychatgpt/study/TryChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trychatgpt.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryChatGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
